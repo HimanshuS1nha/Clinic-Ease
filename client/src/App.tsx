dc0b918b9dc1f1eff12bd4baf09be1f2318c0433
Himanshu Sinha <ashusinha1904@gmail.com>
2024-09-05 22:04:00 +0530

    React Router Dom setup complete

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
-import { Button } from "./components/ui/button";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
-    <div>
-      <Button>Hello</Button>
-    </div>
+    <Routes>
+      <Route path="/" Component={HomePage} />
+    </Routes>
   );
 };
 
