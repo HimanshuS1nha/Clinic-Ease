2bff6435b17621359c22f8ad429c190276e77111
Himanshu Sinha <ashusinha1904@gmail.com>
2024-09-06 22:01:01 +0530

    LoginPage added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
+import LoginPage from "./pages/auth/LoginPage";
+
 const App = () => {
   return (
     <Routes>
       <Route path="/" Component={HomePage} />
+
+      {/* Auth */}
+      <Route path="/login" Component={LoginPage} />
     </Routes>
   );
 };
