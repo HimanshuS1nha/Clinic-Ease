bdb2f98255d5a1658043d05aa676e8eac2ae2091
Himanshu Sinha <ashusinha1904@gmail.com>
2024-09-07 02:57:54 +0530

    HeroSection bug fix

--- client/src/components/home/HeroSection.tsx
+++ client/src/components/home/HeroSection.tsx
@@ -2,28 +2,30 @@ import { Button } from "../ui/button";
 
 const HeroSection = () => {
   return (
-    <main className="flex h-[80vh] items-center justify-center gap-x-12">
-      <div className="w-[50%] flex flex-col justify-center items-center gap-y-6">
-        <div className="flex flex-col gap-y-3">
-          <div className="bg-gray-100 border border-gray-200 rounded-full px-4 py-2 shadow-sm shadow-gray-300 w-fit">
-            <p className="text-xs font-semibold">ClinicEase is now public</p>
+    <main className="flex h-[80vh] items-center justify-center gap-x-8">
+      <div className="w-[50%] flex justify-center items-center">
+        <div className="flex flex-col gap-y-6 pl-16">
+          <div className="flex flex-col gap-y-3">
+            <div className="bg-gray-100 border border-gray-200 rounded-full px-4 py-2 shadow-sm shadow-gray-300 w-fit">
+              <p className="text-xs font-semibold">ClinicEase is now public</p>
+            </div>
+            <h1 className="text-4xl text-emerald-600 font-bold">
+              Your clinic at your Fingertips
+            </h1>
           </div>
-          <h1 className="text-4xl text-emerald-600 font-bold">
-            Your clinic at your Fingertips
-          </h1>
-        </div>
 
-        <p className="text-justify leading-7 pl-32">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto eius
-          suscipit delectus hic consequuntur sapiente ratione earum fugiat
-          dolor. Dolor voluptates iusto sit fugit mollitia cupiditate. Optio
-          eaque animi recusandae! Laudantium qui reprehenderit, expedita
-          praesentium perspiciatis beatae doloremque! Nostrum, doloribus!
-          Incidunt voluptas eaque sint enim illum, vero quod adipisci fugiat.\
-        </p>
+          <p className="text-justify leading-7">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto eius
+            suscipit delectus hic consequuntur sapiente ratione earum fugiat
+            dolor. Dolor voluptates iusto sit fugit mollitia cupiditate. Optio
+            eaque animi recusandae! Laudantium qui reprehenderit, expedita
+            praesentium perspiciatis beatae doloremque! Nostrum, doloribus!
+            Incidunt voluptas eaque sint enim illum, vero quod adipisci fugiat.\
+          </p>
 
-        <div className="w-full pl-32">
-          <Button>Get Started</Button>
+          <div className="w-full">
+            <Button>Get Started</Button>
+          </div>
         </div>
       </div>
       <div className="w-[50%] h-full flex justify-center items-center">
