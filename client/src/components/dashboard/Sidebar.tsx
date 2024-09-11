7528ebbc76f84cfb962427002ebccff45e0d763f
Himanshu Sinha <ashusinha1904@gmail.com>
2024-09-11 21:50:04 +0530

    Sidebar updated

--- client/src/components/dashboard/Sidebar.tsx
+++ client/src/components/dashboard/Sidebar.tsx
@@ -1,5 +1,13 @@
 import { Link } from "react-router-dom";
 import { patientSidebarItems } from "../../../constants/sidebar-items";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Sidebar = () => {
   return (
@@ -15,16 +23,27 @@ const Sidebar = () => {
         );
       })}
 
-      <div className="flex gap-x-4 items-center absolute bottom-4 group hover:bg-emerald-200 py-2 pl-4 pr-8 cursor-pointer -mx-4 rounded-full">
-        <div className="bg-emerald-600 flex items-center justify-center w-10 h-10 rounded-full">
-          <p className="text-white text-lg font-semibold">R</p>
-        </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className="flex gap-x-4 items-center absolute bottom-4 group hover:bg-emerald-200 py-2 pl-4 pr-8 cursor-pointer -mx-4 rounded-full">
+            <div className="bg-emerald-600 flex items-center justify-center w-10 h-10 rounded-full">
+              <p className="text-white text-lg font-semibold">R</p>
+            </div>
 
-        <div className="flex flex-col">
-          <p className="text-lg font-semibold">Random User</p>
-          <p className="text-gray-700 text-sm">User</p>
-        </div>
-      </div>
+            <div className="flex flex-col">
+              <p className="text-lg font-semibold">Random User</p>
+              <p className="text-gray-700 text-sm">User</p>
+            </div>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-40">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="focus:bg-rose-600 focus:text-white cursor-pointer">
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
