f0ea8bc5cf84685f2ef09f9d1cd34866784df5ee
Himanshu Sinha <ashusinha1904@gmail.com>
2024-09-13 16:18:39 +0530

    Responsiveness added to Copyright

--- client/src/components/home/Copyright.tsx
+++ client/src/components/home/Copyright.tsx
@@ -1,6 +1,6 @@
 const Copyright = () => {
   return (
-    <div className="flex justify-center items-center h-[7vh] font-semibold text-lg">
+    <div className="flex justify-center items-center h-[7vh] font-semibold text-sm sm:text-lg">
       Copyright © [2024] by
       <p className="ml-1.5 font-bold">
         Clinic<span className="text-emerald-600">Ease</span>
