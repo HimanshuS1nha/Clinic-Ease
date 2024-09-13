acac73dc0629fe41c7697c9e7dc37434e0a33a76
Himanshu Sinha <ashusinha1904@gmail.com>
2024-09-13 21:29:34 +0530

    AddPatient and BookAppointment pages updated

--- client/src/pages/patient-dashboard/AddPatientPage.tsx
+++ client/src/pages/patient-dashboard/AddPatientPage.tsx
@@ -1,10 +1,45 @@
 import DashboardWrapper from "@/components/dashboard/DashboardWrapper";
 import Title from "@/components/dashboard/Title";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const AddPatientPage = () => {
   return (
     <DashboardWrapper>
       <Title>Add Patient</Title>
+
+      <div className="flex flex-col gap-y-6">
+        <div className="flex flex-col gap-y-3">
+          <Label className="ml-1.5">Name</Label>
+          <Input placeholder="Enter patient's name" />
+        </div>
+        <div className="flex flex-col gap-y-3">
+          <Label className="ml-1.5">Age</Label>
+          <Input placeholder="Enter patient's age" type="number" />
+        </div>
+        <div className="flex flex-col gap-y-3">
+          <Label className="ml-1.5">Gender</Label>
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Select patient's gender" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="male">Male</SelectItem>
+              <SelectItem value="female">Female</SelectItem>
+              <SelectItem value="other">Other</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      <Button>Book</Button>
     </DashboardWrapper>
   );
 };
--- client/src/pages/patient-dashboard/BookAppointmentPage.tsx
+++ client/src/pages/patient-dashboard/BookAppointmentPage.tsx
@@ -17,30 +17,41 @@ const BookAppointmentPage = () => {
   const [date, setDate] = useState<Date>();
   return (
     <DashboardWrapper>
-      {/* <section className="bg-white w-[95%] h-[95%] rounded-xl shadow-lg shadow-gray-300 p-6 flex flex-col gap-y-10"> */}
-        <Title>Book Appointment</Title>
+      <Title>Book Appointment</Title>
 
-        <div className="flex flex-col gap-y-6">
-          <div className="flex flex-col gap-y-3">
-            <Label className="ml-1.5">Doctor</Label>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="Select a doctor" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="abc">ABC</SelectItem>
-                <SelectItem value="def">DEF</SelectItem>
-                <SelectItem value="ghi">GHI</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-          <div className="flex flex-col gap-y-3">
-            <Label className="ml-1.5">Date</Label>
-            <DatePicker date={date} setDate={setDate} />
-          </div>
+      <div className="flex flex-col gap-y-6">
+        <div className="flex flex-col gap-y-3">
+          <Label className="ml-1.5">Patient</Label>
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Select a patient" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="abc">ABC</SelectItem>
+              <SelectItem value="def">DEF</SelectItem>
+              <SelectItem value="ghi">GHI</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
-        <Button>Book</Button>
-      {/* </section> */}
+        <div className="flex flex-col gap-y-3">
+          <Label className="ml-1.5">Doctor</Label>
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Select a doctor" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="abc">ABC</SelectItem>
+              <SelectItem value="def">DEF</SelectItem>
+              <SelectItem value="ghi">GHI</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex flex-col gap-y-3">
+          <Label className="ml-1.5">Date</Label>
+          <DatePicker date={date} setDate={setDate} />
+        </div>
+      </div>
+      <Button>Book</Button>
     </DashboardWrapper>
   );
 };
