b03af8e3bf0fa585219dabfde062d00aa39315dd
Himanshu Sinha <ashusinha1904@gmail.com>
2024-09-13 16:15:34 +0530

    Responsiveness added to ContactUs

--- client/src/components/home/ContactUs.tsx
+++ client/src/components/home/ContactUs.tsx
@@ -9,16 +9,16 @@ const ContactUs = () => {
   return (
     <section
       id="contact"
-      className="flex justify-center items-center gap-x-16 mt-20"
+      className="flex flex-col lg:flex-row justify-center items-center gap-y-16 lg:gap-y-0  gap-x-0lg:gap-x-16 mt-20"
     >
-      <div className="flex flex-col gap-y-7 w-[50%]">
+      <div className="flex flex-col gap-y-7 w-[90%] md:w-[75%] lg:w-[50%]">
         <div className="flex flex-col gap-y-1">
           <p className="text-sm text-gray-700 font-semibold">Contact Us</p>
           <p className="text-5xl text-emerald-600 font-bold">
             Get in touch with Us
           </p>
         </div>
-        <p className="text-justify leading-7 w-[80%]">
+        <p className="text-justify leading-7 w-full md:w-[80%]">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
           accusantium illum similique, quaerat facilis distinctio explicabo
           perspiciatis! Labore enim laboriosam quisquam quo quaerat molestiae
@@ -59,7 +59,7 @@ const ContactUs = () => {
         </div>
       </div>
 
-      <div className="p-5 bg-emerald-600 flex flex-col gap-y-7 w-[30%] rounded-lg mt-6 shadow-xl shadow-gray-300">
+      <div className="p-5 bg-emerald-600 flex flex-col gap-y-7 w-[90%] md:w-[75%] lg:w-[30%] rounded-lg mt-6 shadow-xl shadow-gray-300">
         <Input placeholder="Enter your name" />
         <Input placeholder="Enter your email" />
         <Input placeholder="Enter your phone number" />
